4ea975cd3a9a37b5634ada57de774dc48574d186
zadkiel_troche <troche20001@gmail.com>
2022-03-24 21:30:43 -0300

    Tarea 2

--- tarea 23-03-22/js/ejercicio1.js
+++ tarea 23-03-22/js/ejercicio1.js
@@ -1,1 +1,18 @@
-var tabla_1=["5x1: 5", "5x2: 10", "5x3:15"]
+
+var tabla_mul=[1,2,3,4,5,6,7,8,9,10]
+var num=5;
+
+for(i=0; i<tabla_mul.length; i++){
+    console.log(num+' x '+tabla_mul[i]+' = '+tabla_mul[i]*num)
+}
+
+console.log('\n')
+console.log('Imprimir  las tablas del 6 al 8')
+var num_1=6;
+var num_2=8;
+
+for(i=num_1; i<=num_2; i++){
+    for(j=0; j<tabla_mul.length; j++){
+        console.log(i+' x '+tabla_mul[j]+' = '+tabla_mul[j]*i)
+    }
+}
