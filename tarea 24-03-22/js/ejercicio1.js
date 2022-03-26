00290b070c96ef8f5f07731d29cf8dd83c2727a6
Zadkiel Troche <80989318+zadkiel-troche@users.noreply.github.com>
2022-03-26 09:51:17 -0300

    Update ejercicio1.js

--- tarea 24-03-22/js/ejercicio1.js
+++ tarea 24-03-22/js/ejercicio1.js
@@ -2,12 +2,13 @@
 var tabla_mul=[1,2,3,4,5,6,7,8,9,10]
 var num=5;
 
+console.log('Imprimir la tabla del 5')
 for(i=0; i<tabla_mul.length; i++){
     console.log(num+' x '+tabla_mul[i]+' = '+tabla_mul[i]*num)
 }
 
 console.log('\n')
-console.log('Imprimir  las tablas del 6 al 8')
+console.log('Imprimir las tablas del 6 al 8')
 var num_1=6;
 var num_2=8;
 
