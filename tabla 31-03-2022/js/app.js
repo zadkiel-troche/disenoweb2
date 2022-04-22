bb9b50013fe3a841ad3643c0f80701f05772e4f1
zadkiel_troche <troche20001@gmail.com>
2022-04-21 21:24:47 -0400

    Cargar Registros nuevos y editar registros

--- tabla 31-03-2022/js/app.js
+++ tabla 31-03-2022/js/app.js
@@ -3,7 +3,9 @@ personas=[];
 
 document.getElementById("btn-cargar").onclick=mostrarDatos;
 document.getElementById("btn-limpiar").onclick=limpiarDatos;
-document.getElementById("btn-guardar").onclick=nuevoDato;
+document.getElementById("btn-nuevo").onclick=nuevoRegistro;
+document.getElementById("btn-cancelar").onclick=cancelarForm;
+document.getElementById("btn-guardar").onclick=procesarForm;
 
 window.onload = iniciarApp();
 
@@ -33,17 +35,45 @@ function mostrarDatos(){
         }
 }
 
-function nuevoDato(){
+function cancelarForm(){
+    var form = document.getElementById("formCargar");
+    form.setAttribute("hidden", "");
+}
+
+function nuevoRegistro(){
     document.getElementById("idx").value="-1";
     document.getElementById("num_doc").value="";
     document.getElementById("nombre").value="";
     document.getElementById("apellido").value="";
     document.getElementById("fec_nac").value="";
     document.getElementById("direc").value="";
+
+    document.getElementById("formCargar").removeAttribute("hidden");
+    window.scroll(0, 300);
+    document.getElementById("num_doc").focus();
+}
+
+function procesarForm()
+{
+  let idx=document.getElementById('idx').value;
+  console.log(idx);
+  if (idx==-1)
+    {
+        personas.push({"numci": document.getElementById('num_doc').value, "nombre": document.getElementById('nombre').value, "apellido": document.getElementById('apellido').value, "fec_nac": document.getElementById('fec_nac').value, "direccion": document.getElementById('direc').value});
+    }
+    else {
+        personas[idx].numci=document.getElementById('num_doc').value;
+        personas[idx].nombre=document.getElementById('nombre').value;
+        personas[idx].apellido=document.getElementById('apellido').value;
+        personas[idx].fec_nac=document.getElementById('fec_nac').value;
+        personas[idx].direccion=document.getElementById('direc').value;
+    }
+    cancelarForm();
+    mostrarDatos();
 }
 
 function editarDatos(e){
-    console.log(e.target)
+
     let idxe=e.target.attributes["data-idx"].value;
 
     document.getElementById("idx").value=idxe;
@@ -52,5 +82,16 @@ function editarDatos(e){
     document.getElementById("apellido").value=personas[idxe].apellido;
     document.getElementById("fec_nac").value=personas[idxe].fec_nac;
     document.getElementById("direc").value=personas[idxe].direccion;
+
+    document.getElementById("formCargar").removeAttribute("hidden");
+    window.scroll(0, 500);
+
+}
+
+function borrarDatos(e){
+    console.log(e.target)
+    let idxe=e.target.attributes["data-idx"].value;
+
+    personas.splice(0);
 
 }
